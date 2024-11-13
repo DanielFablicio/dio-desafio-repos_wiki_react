9871dfa822118326f5e7748f1547eb76bcd4d603
danielfablicio <fabliciodaniel@gmail.com>
2024-11-13 19:56:32 -0300

    configurado o basePath fixes #1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV;
+const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+};
 
 export default nextConfig;
